7dabf87dddaa16733e5aa9ac3caa5b05b6a8ab03
vdamas <vitoriodamas@gmail.com>
2023-02-15 12:48:18 +0000

    Add files via upload

---
+++ docker-v1.3.0.0-20170628.sh
@@ -0,0 +1,21 @@
+!#/bin/bash
+
+# Absolute path to this script
+SCRIPT=$(readlink -f "$0")
+
+# Absolute path this script is in
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+tee "$SCRIPTPATH/Dockerfile" > /dev/null <<EOF
+FROM ubuntu:22.04
+
+COPY ./install-v1.3.0.0-20170628.sh /tmp/
+
+RUN chmod +x /tmp/install-v1.3.0.0-20170628.sh
+
+RUN /tmp/install-v1.3.0.0-20170628.sh
+
+EXPOSE 61143
+EOF
+
+docker build -t cryptoescudo:v1.3.0.0-20170628 .
