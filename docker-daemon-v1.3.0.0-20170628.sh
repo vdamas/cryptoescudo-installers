3dd3d39d1035f6230f02665d1b755cbf10c56da7
vdamas <vitoriodamas@gmail.com>
2023-03-14 07:07:21 +0000

    Update and rename docker-v1.3.0.0-20170628.sh to docker-daemon-v1.3.0.0-20170628.sh

--- docker-daemon-v1.3.0.0-20170628.sh
+++ docker-daemon-v1.3.0.0-20170628.sh
@@ -9,11 +9,11 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 tee "$SCRIPTPATH/Dockerfile" > /dev/null <<EOF
 FROM ubuntu:22.04
 
-COPY ./install-v1.3.0.0-20170628.sh /tmp/
+COPY ./install-daemon-v1.3.0.0-20170628.sh /tmp/
 
-RUN chmod +x /tmp/install-v1.3.0.0-20170628.sh
+RUN chmod +x /tmp/install-daemon-v1.3.0.0-20170628.sh
 
-RUN /tmp/install-v1.3.0.0-20170628.sh
+RUN /tmp/install-daemon-v1.3.0.0-20170628.sh
 
 EXPOSE 61143
 EOF
